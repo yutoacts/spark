24fea493aea9161a6d83be3c294076aa0deac1ee
Yuto Akutsu <yuto.akutsu@oss.nttdata.com>
2021-11-05 15:00:21 +0900

    test

--- sql/core/src/main/scala/org/apache/spark/sql/catalyst/analysis/ReplaceCharWithVarchar.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/catalyst/analysis/ReplaceCharWithVarchar.scala
@@ -16,7 +16,7 @@
  */
 
 package org.apache.spark.sql.catalyst.analysis
-
+* test
 import org.apache.spark.sql.catalyst.catalog.CatalogTable
 import org.apache.spark.sql.catalyst.plans.logical.{AddColumns, AlterColumn, CreateV2Table, LogicalPlan, ReplaceColumns, ReplaceTable}
 import org.apache.spark.sql.catalyst.rules.Rule
